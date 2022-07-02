fb8c915bb6d6856782a4f6dc6fa0efbf8e19805a
hiromi_fukuda <fukudahiromumi@hiromi-2.local>
2022-07-02 10:16:17 +0900

    add

--- .bin/brew.sh
+++ .bin/brew.sh
@@ -6,6 +6,21 @@ if [ "$(uname)" != "Darwin" ] ; then
 fi
 
 if [ ! -f /opt/homebrew/bin/brew ]; then
+    echo "installing homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 echo "Homebrew is already installed"
+
+
+formulas=(
+    git
+    ghq
+    tig
+    peco
+    neovim
+)
+
+echo "brew install formula"
+for formula in "${formulas[@]}"; do
+    brew install $formula || brew upgrade $formula
+done
